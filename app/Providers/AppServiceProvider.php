3debc7c59a8486a976480096f6adcfade89ad93c
Marlon81785 <marlon81785@gmail.com>
2020-10-03 01:58:42 -0300

    adicionando suporte ao banco de dados usado no servidor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //adicionado para dar suporte a banco de dados atual
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);//tamanho padrao para banco de dados
     }
 
     /**
